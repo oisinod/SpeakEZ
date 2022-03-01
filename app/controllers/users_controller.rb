3cac087ae1c3f694464cb3dbf5bafb4a3755d8d5
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-01 19:35:52 +0100

    fixing pundit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
+    authorize @user
+    if params[:search].present?
+      @users = User.global_search(params[:search][:query])
+    else
+      @users = User.all
+    end
   end
 
   def show

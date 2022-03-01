cd2b87e457a2ec9c829a51463110e95b1bcc054b
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-01 14:47:48 +0100

    some user seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 # creates the seeds of all the languages
-languages = ["Afrikaans","Albanian","Arabic","Armenian","Basque","Bengali","Bulgarian","Catalan","Cambodian","Chinese (Mandarin)","Croatian","Czech","Danish","Dutch","English","Estonian","Fiji","Finnish","French","Georgian","German","Greek","Gujarati","Hebrew","Hindi","Hungarian","Icelandic","Indonesian","Irish","Italian","Japanese","Javanese","Korean","Latin","Latvian","Lithuanian","Macedonian","Malay","Malayalam","Maltese","Maori","Marathi","Mongolian","Nepali","Norwegian","Persian","Polish","Portuguese","Punjabi","Quechua","Romanian","Russian","Samoan","Serbian","Slovak","Slovenian","Spanish","Swahili","Swedish ","Tamil","Tatar","Telugu","Thai","Tibetan","Tonga","Turkish","Ukrainian","Urdu","Uzbek","Vietnamese","Welsh","Xhosa"]
+languages = ["Afrikaans", "Albanian", "Arabic", "Armenian", "Basque", "Bengali", "Bulgarian", "Catalan", "Cambodian", "Chinese (Mandarin)", "Croatian", "Czech", "Danish", "Dutch", "English", "Estonian", "Fiji", "Finnish", "French", "Georgian", "German", "Greek", "Gujarati", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Javanese", "Korean", "Latin", "Latvian", "Lithuanian", "Macedonian", "Malay", "Malayalam", "Maltese", "Maori", "Marathi", "Mongolian", "Nepali", "Norwegian", "Persian", "Polish", "Portuguese", "Punjabi", "Quechua", "Romanian", "Russian", "Samoan", "Serbian", "Slovak", "Slovenian", "Spanish", "Swahili", "Swedish ", "Tamil", "Tatar", "Telugu", "Thai", "Tibetan", "Tonga", "Turkish", "Ukrainian", "Urdu", "Uzbek", "Vietnamese", "Welsh", "Xhosa"]
 Language.destroy_all
 languages.each do |language|
   Language.create!(name: language)
 end
 
-
-10.times do
-  user = User.new(email: "oisin.odoherty@ucdconnect.ie",
-      username: "oisinod",
-      location: "cologne",
-      first_name: "oisin",
-      last_name: "odoherty",
-      phone: "+353833726941",
-      bio: nil, nationality: "Irish",
-      availability: nil, age: 55)
+User.destroy_all
+10.times do |time|
+  puts time
+  user = User.new(
+    email: "test@#{time}.com",
+    password: 123_456,
+    username: "#{Faker::Books::CultureSeries.planet}#{(1..100).to_a.sample}",
+    location: "cologne",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: "123456789",
+    bio: nil,
+    nationality: "Irish",
+    availability: nil,
+    age: (1..100).to_a.sample
+  )
+  user.save!
 end
-[#<User id: 1, email: "oisin.odoherty@ucdconnect.ie", username: "oisinod", location: "cologne", first_name: "oisin", last_name: "odoherty", phone: "+353833726941", bio: nil, nationality: "Irish", availability: nil, age: 55, created_at: "2022-03-01 13:30:09.291158000 +0000", updated_at: "2022-03-01 13:30:09.291158000 +0000">]

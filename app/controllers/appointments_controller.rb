f6ab48b3303db1d32664abc04fa75ed887682691
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-02 14:30:16 +0100

    added dropdowns for my language and partner's langauge to the appointments form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,9 @@ class AppointmentsController < ApplicationController
 
   def new
     @receiver = User.find(params[:user_id])
+    @asker = current_user
+    @r_languages = @receiver.languages
+    @a_languages = @asker.languages
     @appt = Appointment.new
     authorize @appt
   end
@@ -10,6 +13,7 @@ class AppointmentsController < ApplicationController
     @appt = Appointment.new(appt_params)
     @appt.receiver = User.find(params[:user_id])
     @appt.asker = current_user
+
     if @appt.save!
     #needs to be changed to dashboard once that path excists!
       redirect_to user_path(@appt.receiver)

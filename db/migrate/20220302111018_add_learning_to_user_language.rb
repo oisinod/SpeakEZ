f912d49d7ecf077257ff2b4f34b60c6e4297a465
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-02 12:16:28 +0100

    added the learning / not learning property to the form

--- db/migrate/20220302111018_add_learning_to_user_language.rb
+++ db/migrate/20220302111018_add_learning_to_user_language.rb
@@ -1,5 +1,5 @@
 class AddLearningToUserLanguage < ActiveRecord::Migration[6.1]
   def change
-    add_column :user_languages, :learning, :boolean
+    add_column :user_languages, :learning, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_142438) do
+ActiveRecord::Schema.define(version: 2022_03_02_111018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_142438) do
     t.boolean "validated", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "learning", default: false
     t.index ["language_id"], name: "index_user_languages_on_language_id"
     t.index ["user_id"], name: "index_user_languages_on_user_id"
   end

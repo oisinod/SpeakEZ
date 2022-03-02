1bf85493cd8d1040ee366a2cfc8232f1f508ac49
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-02 12:48:46 +0100

    created routes controller-actions and view for appointment new and create, replaced date with datetime on appointment-model, added pundit policy for appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,27 @@
 class AppointmentsController < ApplicationController
+
+  def new
+    @receiver = User.find(params[:user_id])
+    @appt = Appointment.new
+    authorize @appt
+  end
+
+  def create
+    @appt = Appointment.new(appt_params)
+    @appt.receiver = User.find(params[:user_id])
+    @appt.asker = current_user
+    if @appt.save!
+    #needs to be changed to dashboard once that path excists!
+      redirect_to user_path(@appt.receiver)
+    else
+      render :new
+    end
+    authorize @appt
+  end
+
+  private
+
+  def appt_params
+    params.require(:appointment).permit(:location, :datetime)
+  end
 end

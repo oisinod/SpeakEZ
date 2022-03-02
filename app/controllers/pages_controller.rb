3c883676ff3a1f48cfdf0e5b047f0fce1002eb8f
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-02 12:11:01 +0100

    search function first steps, added migration to user lnaugages table to specify if you're learning a language or speak it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
+    @user_languages = @user.user_languages
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,15 @@ class UsersController < ApplicationController
   def index
     @users = policy_scope(User)
     @user = current_user
-    if params[:search].present?
-      @users = User.global_search(params[:search][:query])
+    if params[:language].present?
+      # Gets the object of the actual language here
+      @language = Language.where(name: params[:language])
+      # Now gets the user languages who have this language
+      @user_language = UserLanguage.where(language: @language)
+      # Now you should return each user associated with the above active record relation
+      @users = @user_language.map do |language|
+        language.user
+      end
     else
       @users = User.all
     end

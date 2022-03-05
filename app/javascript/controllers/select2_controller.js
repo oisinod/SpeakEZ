43e1712e8d3879ce754e82e49eacbe16e4216a38
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-05 23:05:32 +0100

    removed option for users to put nonexistant languages into the form

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -3,9 +3,10 @@ import $ from "jquery";
 import "select2";
 
 const selectbox = document.getElementById("selectbox")
+const searchbox = document.querySelector(".select2-search__field")
 
   $(selectbox).select2(
-    { tags: true,
+    { tags: false,
       theme: 'classic',
       width:'element',
       templateResult: formatFlag,
@@ -14,15 +15,11 @@ const selectbox = document.getElementById("selectbox")
       );
 
       function formatFlag (state) {
-        if (!state.id) {
-          return state.text;
-        }
+        console.log(state.text)
         var baseUrl = "/assets/flags";
         const split = state.text.split(" ")
-
         var $state = $(
           '<span><img src="' + baseUrl + '/' + split[0] + '.svg" class="img-flag"  /> ' + split[1] + '</span>'
         );
-        console.log(state)
         return $state;
       };

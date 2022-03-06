e6c72078148e8a961fd567e75f854726ff75371d
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-06 16:20:33 +0100

    noticed that the association weren't correct anymore, as we have changed that schema - please check carefully if all makes sense

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :appointments_as_asker, class_name: "Appointment", foreign_key: :asker_id
-  has_many :appointments_as_receiver, class_name: "Appointment", foreign_key: :receiver_id
   has_many :user_languages, dependent: :destroy
   has_many :languages, through: :user_languages
+  # not sure the following hast to also specify the "as asker"/"as receiver bit"???
+  has_many :appointments, through: :user_languages
   has_many :messages
   has_one_attached :photo
   validates :username, presence: true, uniqueness: true
   validates :first_name, :last_name, :phone, :nationality, :age, :location, presence: true
   validates :age,  numericality: { only_integer: true }
-  validates :bio, length: {minimum: 5, maximum: 1000}, allow_blank: true
+  validates :bio, length: { minimum: 5, maximum: 1000 }, allow_blank: true
 end

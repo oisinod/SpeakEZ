60497f91784a86a30108210106dd8c60da128053
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-07 10:43:33 +0100

    added * if native language

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_languages, dependent: :destroy
   has_many :languages, through: :user_languages
-  # not sure the following hast to also specify the "as asker"/"as receiver bit"???
-  has_many :appointments, through: :user_languages
   has_many :messages
   has_one_attached :photo
   validates :username, presence: true, uniqueness: true
   validates :first_name, :last_name, :phone, :nationality, :age, :location, presence: true
   validates :age,  numericality: { only_integer: true }
   validates :bio, length: { minimum: 5, maximum: 1000 }, allow_blank: true
+
 end

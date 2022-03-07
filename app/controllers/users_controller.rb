534f5a225a4ece7f1c4038309f4af9cb09187fd9
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-03-07 15:28:46 +0100

    fixed logic on user controllre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = policy_scope(User)
     @user = current_user
     if params[:language].present?
-      @language = Language.find_by(name: params[:language].split(" ")[1])
+      @language = Language.find_by(name: params[:language])
       #gets the instances of the user languages, will then filter by who is learning it or not
       @user_languages = @language.user_languages
       # have all the users which have this language as a user language, now i need to filter by the ones that have learning as false

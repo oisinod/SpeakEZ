20da0b3a9385c059df0d64f9e51751705774122c
Marc Hannemann <hannemann-marc@web.de>
2022-03-08 18:14:32 +0100

    working at styling now

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,10 +3,13 @@ class Chatroom < ApplicationRecord
   has_many :chat_users
   has_many :users, through: :chat_users
 
-  validate :has_unique_users
+  # validate :has_unique_users
 
-  def has_unique_users
-
-  end
-
+  # def has_unique_users
+  #     if self.class.where(user1: self.users[0], user2: self.users[1])
+  #                  .or(self.class.where(user1: self.users[1], user2: self.users[0]))
+  #                  .exists?
+  #       errors.add(:base, 'User 1 and User 2 combination exists!')
+  #     end
+  # end
 end

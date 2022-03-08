63be40ead2fb6c9c2d66267f5fe0a3334b9cb9de
Marc Hannemann <hannemann-marc@web.de>
2022-03-08 16:57:35 +0100

    Hi Andre

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,11 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :chat_users
   has_many :users, through: :chat_users
+
+  validate :has_unique_users
+
+  def has_unique_users
+
+  end
+
 end
